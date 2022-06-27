73cad43242c5e88fb9e9dc7c0aa74ea048b00282
oshani madhushika <oshanimadhushika12@gmail.com>
2022-06-27 13:52:20 +0530

    Added FactoryConfiguration and property of hibernate.cfg

--- src/util/FactoryConfiguration.java
+++ src/util/FactoryConfiguration.java
@@ -1,4 +1,31 @@
 package util;
 
+import entity.Customer;
+import entity.Item;
+import entity.OrderDetail;
+import entity.Orders;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Configuration configuration = new Configuration().configure()
+                .addAnnotatedClass(Customer.class)
+                .addAnnotatedClass(Item.class)
+                .addAnnotatedClass(Orders.class)
+                .addAnnotatedClass(OrderDetail.class);
+
+        sessionFactory=configuration.buildSessionFactory();
+    }
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration==null)? factoryConfiguration=new FactoryConfiguration():factoryConfiguration;
+    }
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
